e9458ff8bf9ece3abd5abc832395757a47dfc76b
jafercedcoss <syedjaferabbasrizvi@cedcoss.com>
2022-09-26 11:39:37 +0530

    JS Variable Exercisse 3

---
+++ src/script.js
@@ -0,0 +1,8 @@
+//  JS Variable Exercise 1 
+var carName = " Volvo";
+//  JS Variable Exercise 2
+var x = 50
+//  JS Variable Exercise 3
+var x= 5;
+var y = 10;
+document.getElementById("demo").innerHTML = x+y;
